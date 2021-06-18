e86bfb7073e64ba28bd38147a624c7fd60253507
franzinBr <alan-franzin2011@live.com>
2021-06-18 09:44:54 -0400

    function prototype

--- modules/ImageMath.hpp
+++ modules/ImageMath.hpp
@@ -4,10 +4,13 @@
 
 #include <stdint.h>
 #include <cmath>
+#include <cstring>
 #include <complex>
 
 void fastFourierTransform(uint32_t n, std::complex<double> x[], std::complex<double>* X);
-
+void inverseFastFourierTransform(uint32_t n, std::complex<double> X[], std::complex<double>* x);
+void discreteFourierTransform2D(uint32_t m, uint32_t n, std::complex<double> x[], std::complex<double>* X);
+void inverseDiscreteFourierTransform2D(uint32_t m, uint32_t n, std::complex<double> X[], std::complex<double>* x);
 
 
 #endif // IMAGEMATH_HPP
